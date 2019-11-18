3802b9ceb57c1a5667166c246d9c25873a6edead
Claire <smith.clairelouise@gmail.com>
2019-11-18 16:02:53 +0000

    ready for deployment

--- config/environment.js
+++ config/environment.js
@@ -2,4 +2,4 @@ const env = process.env.NODE_ENV || 'development'
 const dbURI = `mongodb://localhost/allotted-${env}`
 const secret = 'very secret'
 
-module.exports = { dbURI, port, secret }
+module.exports = { dbURI, secret }
