9a26afb3d5f7853713fff07165489f7c574dbacb
Claire <smith.clairelouise@gmail.com>
2019-12-17 19:22:17 +0000

    ready

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
 const env = process.env.NODE_ENV || 'development'
-const dbURI = process.env.MONGODB_URI || `mongodb://localhost/allotted-${env}`
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/mongolab-adjacent-42136'
 const secret = 'very secret'
 
 module.exports = { dbURI, secret }
